6e2d3b8f0db03030b33e5331731d98e671911803
EricSchles <ericschles@gmail.com>
2014-10-26 12:07:04 -0700

    website grab updated

--- website_grab.py
+++ website_grab.py
@@ -20,18 +20,22 @@ def webpage_grab(url):
         with open(name+now+".html","w") as f:
             f.write(r.text)
         
-def map_website(base_url):
+def map_website(base_url,depth=15):
     r = requests.get(base_url)
     links = []
     name = base_url.split("//")[1].split("/")[0]
-    return mapper(base_url,urlname,links)
+    return mapper(base_url,urlname,links,depth)
 
-def mapper(url,urlname,links):
+def mapper(url,urlname,links,depth):
+    if depth <= 0:
+        return links
     r = requests.get(url)
     html = lxml.html.fromstr(r.text)
     tmp = html.xpath("//a/@href")
     for link in tmp:
         if urlname in link:
-            tmp_links = mapper(link,urlname,links)
-            links.append(link)
-            
+            tmp_links = mapper(link,urlname,links,depth-1)
+            links += [link for link in tmp_links if not link in links]
+            if not link in links:
+                links.append(link)
+    return links
