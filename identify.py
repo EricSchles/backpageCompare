90f7e58ca031bbd3d83e48ea4634695c2137cb91
EricSchles <ericschles@gmail.com>
2014-11-02 11:44:21 -0800

    finished analysis

--- identify.py
+++ identify.py
@@ -18,12 +18,18 @@ negative = pd.DataFrame()
 for folder in folders:
     os.chdir(folder)
     df = pd.read_csv("ny_nj_data.csv")
+    found_emails = []
+    found_numbers = []
     for ind,line in enumerate(df["body"]):
         for email in emails:
             if email in line:
+                found_emails.append(email)
+                email_result = True
                 positive.append(df.iloc[ind],ignore_index=True)
         for phone_number in phone_numbers:
             if phone_number in df.iloc[ind]["phone_number"]:
+                found_numbers.append(phone_number)
+                phone_number_result = True
                 positive.append(df.iloc[ind],ignore_index=True)
         common_keywords = []
         for keyword in keywords:
@@ -38,8 +44,15 @@ for folder in folders:
         with open("analysis.txt","a") as f:
             f.write("for the record: "+line+"\n")
             f.write("the following keywords were found:\n"+common_keywords+"\n")
-            f.write("the naive bayesian classifier, which compares text to determine if they are similar: "+result+"\n")
-
+            f.write("the naive bayesian classifier, which compares text to determine if they are similar, (pos) means similar, (neg) means dissimilar: "+result+"\n")
+            if email_result:
+                f.write("\nThe following email(s) was found in the ad:\n")
+                f.write(found_emails)
+            else:
+                f.write("\nAn email address from the list was not found\n")
+            if phone_number_result:
+                f.write("\nThe following phone number(s) was found in the ad:\n")
+                f.write(found_numbers)
     os.chdir("../")
 
 positive.to_csv("positive.csv")
